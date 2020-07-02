c0116648a5b7548eec0be06ee1cba4121afda33b
linwulong <wulong.lin@iandcode.cn>
2020-07-03 00:29:17 +0800

    防抖-间隔时间内才执行一次, 节流-频繁操作的最后一次才执行

--- src/Throttler/Throttler.js
+++ src/Throttler/Throttler.js
@@ -26,13 +26,43 @@ var Throttler = function () {
     }
 }
 
-function say() {
+function say(e) {
 
-    console.log('hello');
+    console.log('hello', e);
 }
 
-window.addEventListener('scroll', function () {
+function Debounce(fn) {
+
+    if (!Debounce.timer) {
+
+        fn.call(this, arguments[1]);
+        Debounce.timer = setTimeout(() => {
+            Debounce.timer = null;
+        }, 1000);
+    }
+}
+
+window.addEventListener('scroll', function (e) {
 
     Throttler(say);
 
+});
+
+function login() {
+
+    console.log('登陆成功!!!');
+
+}
+
+
+window.addEventListener('keyup', function (e) {
+
+    if (e.keyCode === 13) {
+        Debounce(login);
+    }
 })
+
+getById('login-btn').onclick = function () {
+
+    Debounce(login);
+};
